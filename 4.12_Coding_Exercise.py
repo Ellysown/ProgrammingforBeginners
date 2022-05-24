ad72fe747c5b9d6d858083f89029d7a91f894fd3
Ellyson <xxLAxxjeje09@gmail.com>
2022-05-24 23:54:39 +0800

    Exercise 4.8

---
+++ 4.12_Coding_Exercise.py
@@ -0,0 +1,16 @@
+#John Ellyson Santos
+
+#For Loops
+furniture = ["table", "chair", "cabinet", "desk", "couch"]
+for x in furniture:
+    if x == "cabinet":
+      continue
+    print(x)
+
+#While Loops
+i = 1
+while i < 15:
+    print(i)
+    i += 1
+else:
+    print("i is no longer less than 15.")
